c7959feb5f46bf4f8faf50c1f489268604cb8e51
Jmeniyi <revoded@mail.ru>
2024-05-27 18:24:31 +0300

    complrtr componets "About" and Footer

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,15 +1,101 @@
 <template>
-    <div>about</div>
+  <div class="about">
+    <div class="about__img">
+      <div class="about__img-rama">
+        <img src="../assets/about/rama.png" alt="rama" />
+      </div>
+
+      <div class="about__img-arm">
+        <img src="../assets/about/arm.png" alt="arm" />
+      </div>
+    </div>
+    <div class="about__aboutUS">
+      <h5>Наша команда</h5>
+      <span
+        >Значимость этих проблем настолько очевидна, что базовый вектор развития
+        позволяет оценить значение экспериментов, поражающих по своей
+        масштабности и грандиозности. Мы вынуждены отталкиваться от того, что
+        консультация с широким активом.</span
+      >
+      <div class="about__aboutUS-images">
+        <img src="../assets/about/first.png" alt="first" />
+        <img src="../assets/about/second.jpg" alt="second" />
+        <img src="../assets/about/third.jpg" alt="third" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'aboutComponent',
+  name: "aboutComponent",
 });
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
+.about {
+  display: flex;
+  height: 574px;
+  width: 1440px;
+  margin: auto;
+  position: relative;
+  margin-top: 50px;
 
+  .about__img {
+    width: 40%;
+    height: max-content;
+    display: grid;
+    position: relative;
+    padding-top: 50px;
+  }
+  .about__img-rama {
+    grid-column: 1;
+    grid-row: 1;
+    padding-top: 70px;
+    z-index: 1;
+    width: 100%;
+    img {
+      width: 500px;
+    }
+  }
+  .about__img-arm {
+    grid-column: 1;
+    grid-row: 1;
+    z-index: 2;
+    margin-left: 55px;
+    margin-top: -30px;
+    img {
+      width: 310px;
+    }
+  }
+  .about__aboutUS {
+    width: 50%;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    h5 {
+      font-family: base-font;
+      color: #2c2d35;
+      font-size: 40px;
+      line-height: 90px;
+      font-weight: 500;
+    }
+    span {
+      font-family: base-font;
+      color: #2c2d35;
+      font-size: 18px;
+      line-height: 28px;
+      font-weight: 500;
+      text-align: left;
+    }
+    .about__aboutUS-images{
+      display: flex;
+      margin-top: 20px;
+      gap: 20px;
+    }
+  }
+}
 </style>
--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,15 +1,172 @@
 <template>
-    <div>footer</div>
+  <div class="footer">
+    <div class="footer__first">
+      <div class="footer__first-logo">
+        <img alt="logo" src="../assets/logo.png" />
+        <span>Ink. House</span>
+      </div>
+      <h5>+7 (999) 543-54-54</h5>
+      <h6>Мастерская</h6>
+    </div>
+    <div class="footer-second">
+      <h5>Репродукции</h5>
+      <h6>Франция</h6>
+      <h6>Германия</h6>
+      <h6>Англия</h6>
+    </div>
+    <div class="footer-third">
+      <h5>Новинки</h5>
+      <h6>2021</h6>
+      <h6>2022</h6>
+    </div>
+    <div class="footer-fourth">
+      <h5>О нас</h5>
+      <h6>Художники</h6>
+      <h6>Менеджеры</h6>
+    </div>
+    <div class="footer-fifth">
+      <div class="footer-fifth-social">
+        <img src="../assets/footer/Frame.svg" alt="facebook" />
+        <img src="../assets/footer/Frame2.svg" alt="instagram" />
+        <img src="../assets/footer/Frame3.svg" alt="youtube" />
+      </div>
+      <span
+        >Ink. House ® <br />
+        All rights reserved</span
+      >
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'footerComponent',
+  name: "footerComponent",
 });
 </script>
 
-<style lang="scss">
-
+<style  scoped lang="scss">
+.footer {
+  height: 180px;
+  background-color: rgba(225, 237, 230, 1);
+  width: 1440px;
+  margin: auto;
+  display: grid;
+  grid-template-columns: 25% 10% 10% 10% 25%;
+  justify-content: center;
+  align-content: center;
+  justify-items: center;
+  .footer__first {
+    gap: 8px;
+    display: flex;
+    flex-direction: column;
+    .footer__first-logo {
+      gap: 8px;
+      display: flex;
+      align-items: center;
+      margin-bottom: 20px;
+    }
+    img {
+      height: 45px;
+      width: 45px;
+    }
+    span {
+      font-family: base-font;
+      color: #598d66;
+      font-size: 20px;
+      font-weight: 500;
+      line-height: 25px;
+    }
+    h5 {
+      font-family: base-font;
+      color: rgba(44, 45, 53, 1);
+      font-size: 20px;
+      line-height: 25px;
+      font-weight: 500;
+    }
+    h6 {
+      font-family: base-font;
+      color: rgba(134, 146, 139, 1);
+      font-size: 18px;
+      line-height: 20px;
+      font-weight: 500;
+    }
+  }
+  .footer-second {
+    gap: 8px;
+    display: flex;
+    flex-direction: column;
+    h5 {
+      font-family: base-font;
+      color: rgba(44, 45, 53, 1);
+      font-size: 20px;
+      line-height: 25px;
+      font-weight: 500;
+    }
+    h6 {
+      font-family: base-font;
+      color: rgba(134, 146, 139, 1);
+      font-size: 18px;
+      line-height: 27px;
+      font-weight: 500;
+    }
+  }
+  .footer-third {
+    gap: 8px;
+    display: flex;
+    flex-direction: column;
+    h5 {
+      font-family: base-font;
+      color: rgba(44, 45, 53, 1);
+      font-size: 20px;
+      line-height: 25px;
+      font-weight: 500;
+    }
+    h6 {
+      font-family: base-font;
+      color: rgba(134, 146, 139, 1);
+      font-size: 18px;
+      line-height: 27px;
+      font-weight: 500;
+    }
+  }
+  .footer-fourth {
+    gap: 8px;
+    display: flex;
+    flex-direction: column;
+    h5 {
+      font-family: base-font;
+      color: rgba(44, 45, 53, 1);
+      font-size: 20px;
+      line-height: 25px;
+      font-weight: 500;
+    }
+    h6 {
+      font-family: base-font;
+      color: rgba(134, 146, 139, 1);
+      font-size: 18px;
+      line-height: 27px;
+      font-weight: 500;
+    }
+  }
+  .footer-fifth {
+    .footer-fifth-social {
+      gap: 10px;
+      display: flex;
+      margin-bottom: 10px;
+    }
+    img {
+      height: 25px;
+      width: 25px;
+    }
+    span {
+      font-family: base-font;
+      color: rgba(44, 45, 53, 1);
+      font-size: 12px;
+      line-height: 25px;
+      font-weight: 500;
+    }
+  }
+}
 </style>
